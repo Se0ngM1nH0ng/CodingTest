bceb78cc16626fd7d75821e120f714f6c56c75ee
seongminHong1 <137602339+seongminHong1@users.noreply.github.com>
2023-11-02 09:25:19 +0900

    Update 특수문자 출력하기.java

--- 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Solution {
     public static void main(String[] args) {
         System.out.println("!@#$%^&*(\\\'\"<>?:;");
+        //  작은 따옴표 ' 나 큰 따옴표 " 출력시 앞에 "\" 붙여준다.
+        
+        // 작은 따옴표 : \'
+        // 큰 따옴표 : \"
     }
 }
